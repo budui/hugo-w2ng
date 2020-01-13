0f460932d882bb92dcffbf5c6b92331ab7889ea9
wangbudui <wangbudui@foxmail.com>
2020-01-13 19:15:40 +0800

    :tada: hugo-w2ng 1.0.0

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      'sans': ['Optima', 'Candara', '"Lucida Sans"', 'Calibri', 'Arial','Noto Serif SC', 'source-han-serif-sc', '"Source Han Serif SC"',
+       '"Source Han Serif CN"', '"Source Han Serif TC"', '"Source Han Serif TW"', '"Source Han Serif"', '"Songti SC"', '"Microsoft YaHei"', "sans-serif"],
+      'mono': ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'SFMono-Regular', 'monospace'],
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}

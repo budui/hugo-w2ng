c33b1e4bbb3fd2951e75b3dc93392d865a54793f
Budui Wang <wangbudui@foxmail.com>
2020-01-26 23:47:11 +0800

    update font setting

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   theme: {
     fontFamily: {
-      'sans': ['Optima', 'Candara', '"Lucida Sans"', 'Calibri', 'Arial','Noto Serif SC', 'source-han-serif-sc', '"Source Han Serif SC"',
-       '"Source Han Serif CN"', '"Source Han Serif TC"', '"Source Han Serif TW"', '"Source Han Serif"', '"Songti SC"', '"Microsoft YaHei"', "sans-serif"],
+      'sans': ['Optima', 'Candara', '"Lucida Sans"', 'Calibri', 'Arial',
+      'source-han-serif-sc', '"Source Han Serif SC"', '"Source Han Serif"',
+      '"Noto Serif CJK SC"', '"Noto Serif SC"',
+      '"Songti SC"', '"Microsoft YaHei"', "sans-serif"],
       'mono': ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'SFMono-Regular', 'monospace'],
     },
     extend: {}
